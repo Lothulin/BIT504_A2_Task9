f926ea0d65fe264815518062400f199d7e5ce8b2
Lothulin <danikat@nowmail.co.nz>
2025-05-06 18:05:33 +1200

    Add files via upload

---
+++ task9example.java
@@ -0,0 +1,12 @@
+package src;
+
+public class task9example {
+
+	public static void main(String[] args) {
+		System.out.println("Here is some code for the test.");
+		System.out.println("Here is some more code for the test.");
+		System.out.println("Here is even more code for the test.");
+
+	}
+
+}
